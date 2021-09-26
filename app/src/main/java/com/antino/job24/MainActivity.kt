3f26488cf1e11814c3123adeaed0c856662bdb1e
akhilesh2020 <itakhilesh2020@gmail.com>
2021-09-26 19:22:17 +0530

    backpress issue resolved

--- app/src/main/java/com/antino/job24/MainActivity.kt
+++ app/src/main/java/com/antino/job24/MainActivity.kt
@@ -2,14 +2,21 @@ package com.antino.job24
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.antino.job24.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -20,6 +27,23 @@ class MainActivity : AppCompatActivity() {
 
     }
     private fun initView(){
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
 
     }
+
+    override fun onBackPressed() {
+        val des= navController.currentDestination?.id ?:-1
+        when (des) {
+            R.id.nav_rdashboard -> {
+
+            }
+            R.id.nav_cdashboard -> {
+
+            }
+            else -> {
+                super.onBackPressed()
+            }
+        }
+    }
+
 }
